ff386507a2fc4155c5201dbbd18b1de86867e013
aid003 <teremochek859@gmail.com>
2023-11-09 18:14:10 +0300

    ывапмимпавыапмирпа

--- src/app/layout.js
+++ src/app/layout.js
@@ -34,23 +34,28 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <HeaderDesktop></HeaderDesktop>
         <div className={styles.layoutContainer}>{children}</div>
-        <Script id="metrika-counter">
-          {`(function(m,e,t,r,i,k,a){m[i]=m[i]function(){(m[i].a=m[i].a[]).push(arguments)};
-              m[i].l=1*new Date();
-              for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-              (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-              ym(95508734, "init", {
-              clickmap:true,
-              trackLinks:true,
-              accurateTrackBounce:true,
-              webvisor:true
-            });`}
-        </Script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+                (function(m,e,t,r,i,k,a){m[i]=m[i]function(){(m[i].a=m[i].a[]).push(arguments)};
+                m[i].l=1*new Date();
+                for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+                (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+      
+                ym(95508734, "init", {
+                      clickmap:true,
+                      trackLinks:true,
+                      accurateTrackBounce:true,
+                      webvisor:true
+                });
+              `,
+          }}
+        />
         <noscript>
           <div>
-            <Image
+            <img
               src="https://mc.yandex.ru/watch/95508734"
               style="position:absolute; left:-9999px;"
               alt=""
