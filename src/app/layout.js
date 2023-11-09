6e617b346c28cdcfda41bdb601ec459f896e79c2
aid003 <teremochek859@gmail.com>
2023-11-09 18:54:17 +0300

    asdxcfvgbn

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,6 +6,7 @@ import Head from "next/head";
 import Script from "next/script";
 import Image from "next/image";
 import { YandexMetricaProvider } from "next-yandex-metrica";
+import { YandexMetrika } from "@/components/metrica/Matrica";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -34,17 +35,23 @@ export default function RootLayout({ children }) {
       </Head>
       <body className={inter.className}>
         <HeaderDesktop></HeaderDesktop>
-        <YandexMetricaProvider
-          tagID={95508734}
-          initParameters={{
-            clickmap: true,
-            trackLinks: true,
-            accurateTrackBounce: true,
-            webvisor: true,
-          }}
-        >
-        </YandexMetricaProvider>
-          <div className={styles.layoutContainer}>{children}</div>
+        <div className={styles.layoutContainer}>{children}</div>
+        <Script id="metrika-counter" strategy="afterInteractive">
+          {`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+              (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+ 
+              ym(95508734, "init", {
+                    defer: true,
+                    clickmap:true,
+                    trackLinks:true,
+                    accurateTrackBounce:true,
+                    webvisor:true
+              });`}
+        </Script>
+        <YandexMetrika />
       </body>
     </html>
   );
